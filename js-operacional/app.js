f45b27be886a0f574aaae56f09a74c29978d2707
Ara Girett <aragirett@gmail.com>
2024-07-30 08:01:33 -0400

    actualizacion

---
+++ js-operacional/app.js
@@ -0,0 +1,32 @@
+/*
+DOMContentLoaded sirve para controlar
+que primero se cargue TODO el docmento
+HTML, por ultimo javascript
+*/
+document
+.addEventListener("DOMContentLoaded", function(){
+    //rsta es zona segura  
+    //console.log(document.getElementById("txtNombre").value);
+    //console.log(document.getElementById("txtApellido").value);
+});
+
+//añadiendo evento click al boton mostrar
+const btnMostrar = document.getElementById("btnMostrar");
+btnMostrar.addEventListener("click", function(){
+    //console.log('haciendo click');
+    //alert("hice click");
+    //recuperando info del formulario
+    
+    const nombres = document.getElementById("txtNombre").value;
+    const apellido = document.getElementById("txtApellido").value;
+    const cedula = document.getElementById("txtCedula").value;
+    const direccion = document.getElementById("txtDireccion").value;
+    console.log('nombres' + nombres + ',apellidos' + apellido + ',cedula' + cedula + 'direccion' + direccion);
+    alert('nombres' + nombres + ',apellidos' + apellido + ',cedula' + cedula + 'direccion' + direccion);
+});
+
+
+function hola(nombre, apellido) {
+    console.log(nombre, apellido);
+    //codigo reutilizable
+}
